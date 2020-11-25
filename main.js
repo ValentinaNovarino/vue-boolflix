78e5cf7ce0063cea373b57d20d0668ca32d5beca
ValentinaNovarino <valenovarino@gmail.com>
2020-11-25 15:53:00 +0100

    aggiunto dettagli milestone 1

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
 var app = new Vue ( {
     el: '#root',
     data: {
+        // array films
         films: [],
+        // input di ricerca
         query: "",
+        // chiave per salvare la ricerca nell'input
+        textResearch: '',
+        // chiave per ricerca in corso
+        researchInProgress: false,
+        // voto massimo in base 5
         maxVote: 5,
+        // array bandiere disponibili
         flags: ['de', 'it', 'fr', 'en', 'es']
     },
     methods: {
         search() {
-            axios.get('https://api.themoviedb.org/3/search/movie', {params: {
-                api_key: '5b23704b41ce9ad797a67a023b27566c',
-                query: this.query}
-            })
-            .then((film) => {
-                this.films = film.data.results;
-                // console.log(this.films);
-                this.films.forEach((film) => {
-                    Math.round(film.vote_average / 2);
+            // eseguo la ricerca che gestisce spazio e stringa vuota
+            if (this.query.trim() != '') {
+                // porto la ricerca in corso su true
+                this.researchInProgress = true;
+                // salvo la ricerca nella chiave
+                this.textResearch = this.query;
+                // chiamata per i film
+                axios.get('https://api.themoviedb.org/3/search/movie', {params: {
+                    api_key: '5b23704b41ce9ad797a67a023b27566c',
+                    query: this.query}
+                })
+                .then((film) => {
+                    this.films = film.data.results;
+                    // console.log(this.films);
+                    // ciclo array film e porto voto in base 5 senza modificare valore iniziale
+                    this.films.forEach((film) => {
+                        Math.round(film.vote_average / 2);
+                    });
+                    // riporto la ricerca in corso su false 
+                    this.researchInProgress = false
+                    // ripulisco input
+                    this.query = "";
                 });
-                this.query = "";
-            });
+            }
         },
     }
 })
